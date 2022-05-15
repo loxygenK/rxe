73730a6c771810d7469a97d759eedee32648f1f7
loxygenk <me@loxygen.dev>
2022-05-15 23:10:35 +0900

    🧑💻 Add new `map` macro for creating macro easily

---
+++ src/macros.rs
@@ -0,0 +1,35 @@
+#[macro_export]
+macro_rules! map {
+    ( <$kt:ty, $vt:ty> ) => {
+        HashMap::<$kt, $vt>::new()
+    };
+    ( <$kt:ty, $vt:ty>; $( $k:expr => $v:expr ), + $(,)? ) => {
+        {
+            let mut temp_map: HashMap<$kt, $vt> = HashMap::new();
+            $(
+                temp_map.insert($k, $v);
+            )*
+            temp_map
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    #[test]
+    fn macro_generates_supposed_map() {
+        assert_eq!(map!(<i8, i8>), HashMap::new());
+        assert_eq!(
+            map!(<i8, i8>; 1 => 1, 2 => 2, 3 => 3),
+            {
+                let mut map = HashMap::new();
+                map.insert(1, 1);
+                map.insert(2, 2);
+                map.insert(3, 3);
+                map
+            }
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod config;
 mod domain;
+mod macros;
 
 fn main() {
     println!("Hello, world!");
