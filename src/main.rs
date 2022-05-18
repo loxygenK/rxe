e345c31d06d6c799ce0e8e5328ab63f9bac20346
loxygenK <me@loxygen.dev>
2022-05-18 12:43:20 +0900

    ✨ Create the module and trait stuffs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod config;
 mod command;
 mod domain;
 mod macros;
+mod runner;
 mod util;
 
 fn main() {
---
+++ src/runner/mod.rs
@@ -0,0 +1,26 @@
+mod nix;
+mod win;
+
+use std::io::Error;
+
+#[cfg(target_family = "windows")]
+pub use win::*;
+
+#[cfg(target_family = "unix")]
+pub use nix::*;
+
+#[cfg(not(any(
+    target_family = "window",
+    target_family = "unix"
+)))]
+compile_error!("This crate cannot be build for the platform which is not either of windows or unix.");
+
+pub type ExitCode = u8;
+
+pub enum ExecuteError {
+    PreparationFailure(Option<Error>)
+}
+
+pub trait CommandRunner {
+    fn run_command(&self, line: &str) -> Result<(), ExecuteError>;
+}
