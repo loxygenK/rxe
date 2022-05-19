025b774eba7f36bf5e5d3b76df2979d4fb8ede30
loxygenk <me@loxygen.dev>
2022-05-19 11:03:14 +0900

    ✅ Add test for `Constraint::convert_value`

--- src/domain/constraint.rs
+++ src/domain/constraint.rs
@@ -15,7 +15,7 @@ pub enum Constraint {
     Choice(Vec<String>)
 }
 impl Constraint {
-    pub fn convert_value(&self, raw_str: &impl ToString) -> Result<ArgumentValue, ConstraintValidateError> {
+    pub fn convert_value(&self, raw_str: impl ToString) -> Result<ArgumentValue, ConstraintValidateError> {
         let raw_str = raw_str.to_string();
 
         match self {
@@ -37,5 +37,86 @@ impl Constraint {
                 Ok(ArgumentValue::Choice(matched[0].to_owned()))
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::ArgumentValue;
+    use super::{Constraint, ConstraintValidateError};
+
+    use rstest::rstest;
+
+    type ConvertResult<T> = Result<T, ConstraintValidateError>;
+
+    #[rstest(input, expected,
+        case("123", Ok("123")),
+        case("", Ok("")),
+        case("Whatever", Ok("Whatever")),
+    )]
+    fn accept_any_string_for_constraint_text(input: &str, expected: ConvertResult<&str>) {
+        assert_eq!(
+            Constraint::Text.convert_value(input),
+            expected.map(|e| ArgumentValue::Text(e.to_string()))
+        );
+    }
+
+    #[rstest(input, expected,
+        case("123", Err(ConstraintValidateError::FlagValue)),
+        case("", Err(ConstraintValidateError::FlagValue)),
+        case("Whatever", Err(ConstraintValidateError::FlagValue)),
+    )]
+    fn accept_no_string_for_constraint_flag(input: &str, expected: ConvertResult<&str>) {
+        assert_eq!(
+            Constraint::Flag.convert_value(input),
+            expected.map(|e| ArgumentValue::Flag(true))
+        );
+    }
+
+    #[rstest(input, expected,
+        case("123", Ok(123f64)),
+        case("", Err(ConstraintValidateError::ConstraintUnmet)),
+        case("Whatever", Err(ConstraintValidateError::ConstraintUnmet)),
+    )]
+    fn accept_numeric_string_for_constraint_flag(input: &str, expected: ConvertResult<f64>) {
+        assert_eq!(
+            Constraint::Number.convert_value(input),
+            expected.map(ArgumentValue::Number)
+        );
+    }
+
+    #[rstest(input, expected,
+        case("ChoiceAAA", Ok("ChoiceAAA")),
+        case("ChoiceBBB", Ok("ChoiceBBB")),
+        case("ChoiceCCC", Ok("ChoiceCCC")),
+        case("Choice", Err(ConstraintValidateError::AmbiguosChoice)),
+        case("", Err(ConstraintValidateError::AmbiguosChoice)),
+        case("Whatever", Err(ConstraintValidateError::AmbiguosChoice)),
+    )]
+    fn accept_string_in_candicate_for_constraint_choice(input: &str, expected: ConvertResult<&str>) {
+        let candicates = vec!["ChoiceAAA", "ChoiceBBB", "ChoiceCCC"].iter()
+            .map(ToString::to_string)
+            .collect();
+
+        assert_eq!(
+            Constraint::Choice(candicates).convert_value(input),
+            expected.map(|e| ArgumentValue::Choice(e.to_string()))
+        );
+    }
+
+    #[rstest(input, expected,
+        case("ChoiceA", Ok("ChoiceAAA")),
+        case("ChoiceB", Ok("ChoiceBBB")),
+        case("ChoiceC", Ok("ChoiceCCC")),
+    )]
+    fn accept_ambiguos_for_constraint_choice(input: &str, expected: ConvertResult<&str>) {
+        let candicates = vec!["ChoiceAAA", "ChoiceBBB", "ChoiceCCC"].iter()
+            .map(ToString::to_string)
+            .collect();
+
+        assert_eq!(
+            Constraint::Choice(candicates).convert_value(input),
+            expected.map(|e| ArgumentValue::Choice(e.to_string()))
+        );
     }
 }
