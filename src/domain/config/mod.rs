e1aad9455fefc61ba8cd890840e875f4475bb29c
loxygenk <me@loxygen.dev>
2022-05-19 10:38:47 +0900

    🚚 Flatten the `domain` module

--- src/domain/config/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod argument;
-pub mod command;
-pub mod config;
-pub mod constraint;
-
-pub use argument::*;
-pub use command::*;
-pub use config::*;
-pub use constraint::*;
--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,5 +1,13 @@
+pub mod argument;
+pub mod argument_value;
+pub mod command;
 pub mod config;
-pub mod inputted;
+pub mod constraint;
+pub mod input_command;
 
+pub use argument::*;
+pub use argument_value::*;
+pub use command::*;
 pub use config::*;
-pub use inputted::*;
+pub use constraint::*;
+pub use input_command::*;
